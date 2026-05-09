5c8ea415d0f07c8a9972cbd069e9e02b2570af77
basadashub <mat.mails@yahoo.com>
2026-05-08 22:30:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
+const CACHE_NAME = 'basadas-v1';
+const ASSETS = [
+  '/',
+  '/index.html', // o tu archivo principal
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  console.log('PWA activa');
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
